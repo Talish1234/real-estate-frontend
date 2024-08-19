05333b945d35c39839e83ca4fa898f0072df99c7
Talish1234 <92112059+Talish1234@users.noreply.github.com>
2024-08-19 17:35:48 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,18 +2,9 @@ import { useContext, useState } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
-import { useNotificationStore } from "../../lib/notificationStore";
 function Navbar(){
   const [open,setOpen] = useState(false);
   const {currentUser} = useContext(AuthContext);
-  
- 
-  const fetch = useNotificationStore((state) => state.fetch);
-  const number = useNotificationStore((state) => state.number);
-
-  if(currentUser) {
-    fetch();
-  }
   return (
       <nav>
           <div className="left">
@@ -31,9 +22,7 @@ function Navbar(){
                <img src={currentUser.avatar} alt=''/>
                <span>{currentUser.username}</span>
                <Link to="/profile" className='profile'>
-                {number > 0 && <div className='notification'>{number}</div>
-
-                }
+                
                 <span>Profile</span>
                </Link>
             </div>):(<div className="sign">
